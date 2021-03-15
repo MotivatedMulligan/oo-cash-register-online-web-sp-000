c30b06628c24b6582fd07ede9ba42d29fb05506f
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-15 23:47:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+  attr_accessor   :total
+@@all = []
+  def initialize
+    total = 0
+    @total = total
+   #expect(cash_register_with_discount.discount).to eq(20
+  end
 
+def total(total)
+ total
+end
+def add_item(title, price, quantity)
+
+end
+def apply_discount
+
+end
+
+def items
+  @@all
+end
+def void_last_transaction
+  self.
+end
+
+
+end
+end
